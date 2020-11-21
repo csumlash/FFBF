8a543c79f87534897af90fe155def4acd573294d
Vivi <meowsachi@gmail.com>
2020-11-21 19:26:09 +0000

    Vivi 3. szinkron layout

--- app/src/main/java/com/example/forfoodiesbyfoodies/AboutStreetFood.java
+++ app/src/main/java/com/example/forfoodiesbyfoodies/AboutStreetFood.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ViewRestaurant extends AppCompatActivity {
+public class AboutStreetFood extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
