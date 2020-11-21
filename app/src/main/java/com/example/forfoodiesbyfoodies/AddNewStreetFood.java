d95dd9c47712d63f3fc18237ffde51df109a036a
Vivi <meowsachi@gmail.com>
2020-11-21 00:45:44 +0000

    Vivi 2. szinkron layouts

---
+++ app/src/main/java/com/example/forfoodiesbyfoodies/AddNewStreetFood.java
@@ -0,0 +1,14 @@
+package com.example.forfoodiesbyfoodies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewStreetFood extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_street_food);
+    }
+}
