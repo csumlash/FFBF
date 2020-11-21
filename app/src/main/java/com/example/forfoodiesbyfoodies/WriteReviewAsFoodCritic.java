c74fe9a84e3943ed8ba5b383bb1857aa3868f5a0
Szabi <szabolcs.kokai@gmail.com>
2020-11-22 00:45:55 +0100

    1. sync - list of restaurants

---
+++ app/src/main/java/com/example/forfoodiesbyfoodies/WriteReviewAsFoodCritic.java
@@ -0,0 +1,14 @@
+package com.example.forfoodiesbyfoodies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WriteReviewAsFoodCritic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_review_as_food_critic);
+    }
+}
