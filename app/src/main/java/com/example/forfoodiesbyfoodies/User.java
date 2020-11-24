1d34cf308fcbf961f086bd1ca625b5a31d72d574
Csumpi <csumlash@gmail.com>
2020-11-24 13:17:02 +0100

    Csumpi - adding User class

---
+++ app/src/main/java/com/example/forfoodiesbyfoodies/User.java
@@ -0,0 +1,44 @@
+package com.example.forfoodiesbyfoodies;
+
+public class User {
+    String username, firstName, lastName, userType;
+
+    public User(String username, String firstName, String lastName, String userType) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userType = userType;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+}
