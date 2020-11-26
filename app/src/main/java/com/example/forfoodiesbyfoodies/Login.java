7af4b63cc8b333e8d40b5adf55daa7223d1a93ca
Csumpi <csumlash@gmail.com>
2020-11-26 15:08:45 +0100

    Csumpi - Registration and Login activity workouts, changing manifest to globally allow keyboard to push the content up and let it be visible.

--- app/src/main/java/com/example/forfoodiesbyfoodies/Login.java
+++ app/src/main/java/com/example/forfoodiesbyfoodies/Login.java
@@ -2,28 +2,61 @@ package com.example.forfoodiesbyfoodies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
-    EditText logEmail, logPw;
-    Button login, logRegister;
-    TextView logWarning, logRegMessage;
+    // The following attributes will contain information are mandatory to perform a login
+    EditText username, psw;
+    // These button attributes will listen to the user interaction to do the login or registration process
+    Button login, register;
+    // The next attributes will be responsible to interact with the user if anything needs more attention
+    TextView loginWarningMsg, registerMsg;
+    // The attributes below will be technical ones to let the code perform actions and accept data to/from databases
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        logEmail = findViewById(R.id.et_log_email);
-        logPw = findViewById(R.id.et_log_password);
+        // Linking the activity GUI views/elements to this code
+        username = findViewById(R.id.et_log_email);
+        psw = findViewById(R.id.et_log_password);
         login = findViewById(R.id.btn_log_login);
-        logWarning = findViewById(R.id.tv_log_warning);
-        logRegMessage = findViewById(R.id.tv_log_regmessage);
-        logRegister = findViewById(R.id.btn_log_register);
+        loginWarningMsg = findViewById(R.id.tv_log_warning);
+        registerMsg = findViewById(R.id.tv_log_regmessage);
+        register = findViewById(R.id.btn_log_register);
+
+        // the login button object performs the declared actions in the login.setonClickListener(...){...};
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // local variables to store and then check the values are entered by the user
+                String enteredName = username.getText().toString();
+                String enteredPsw = username.getText().toString();
+                // logical evaluation of username and password
+                if (enteredName.length() > 0 && enteredPsw.length() > 0){
+                    loginWarningMsg.setText("Looks fine!");
+                } else {
+                    loginWarningMsg.setText("E-mail & Password are mandatory to login!");
+                }
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toRegister = new Intent(Login.this, Registration.class);
+                startActivity(toRegister);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/forfoodiesbyfoodies/Registration.java
+++ app/src/main/java/com/example/forfoodiesbyfoodies/Registration.java
@@ -1,6 +1,8 @@
 package com.example.forfoodiesbyfoodies;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
@@ -9,16 +11,30 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * This Registration class is responsible to insert new users into the Firebase authorisation
+ * database to check if a username (email address) is already registered in the database and
+ * to handle registration process error such as network issues, missing mandatory details. */
 public class Registration extends AppCompatActivity {
 
-    EditText email, firstName, lastName, password;
-    TextView emailWarning, firstNameWarning, lastNameWarning, passwordWarning;
-    Button register;
-    TextWatcher emailTextWatcher;
-    DatabaseReference dbRef;
+    // These attributes are essential user details
+    private EditText email, firstName, lastName, password;
+    // These attributes for feedback to the user (errors, missing details)
+    private TextView emailWarning, firstNameWarning, lastNameWarning, passwordWarning;
+    // This attribute stands for the "register" button to start different database and evaluation tasks
+    private Button register;
+
+    // Getting the reference to the "users" node in the database to query and insert
+    // The following attributes assist for database and other technical workouts
+    private DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("users");
+    private FirebaseAuth auth;
 
 
     @Override
@@ -26,8 +42,18 @@ public class Registration extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        // Linking the views of activity to this code
         email = findViewById(R.id.et_registration_email);
-        /*emailTextWatcher = new TextWatcher() {
+        emailWarning = findViewById(R.id.tv_registration_email_warning);
+        firstName = findViewById(R.id.et_registration_firstname);
+        firstNameWarning = findViewById(R.id.tv_registration_firstname_warning);
+        lastName = findViewById(R.id.et_registration_lastname);
+        lastNameWarning = findViewById(R.id.tv_registration_lastname_warning);
+        password = findViewById(R.id.et_registration_password);
+        passwordWarning = findViewById(R.id.tv_registration_password_warning);
+        register = findViewById(R.id.btn_registration_register);
+
+        email.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -40,37 +66,72 @@ public class Registration extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                if( !email.getText().toString().contains("@")){
-                    emailWarning.setText("@ is essential in e-mails");
-                }else{
-                    emailWarning.setText("looks fine!");
+                if(!isLogicalEmail(email.getText().toString())){
+                    emailWarning.setText("is not looking like a valid e-mail yet");
+                } else {
+                    emailWarning.setText("Thank you!");
+                    emailWarning.setTextColor(getResources().getColor(R.color.teal_700));
                 }
-            }
-        };
-        email.addTextChangedListener(emailTextWatcher);*/
-        emailWarning = findViewById(R.id.tv_registration_email_warning);
-        firstName = findViewById(R.id.et_registration_firstname);
-        firstNameWarning = findViewById(R.id.tv_registration_firstname_warning);
-        lastName = findViewById(R.id.et_registration_lastname);
-        lastNameWarning = findViewById(R.id.tv_registration_lastname_warning);
-        password = findViewById(R.id.et_registration_password);
-        passwordWarning = findViewById(R.id.tv_registration_password_warning);
-        register = findViewById(R.id.btn_registration_register);
-
-        dbRef = FirebaseDatabase.getInstance().getReference("users");
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                User user = new User(
-                        email.getText().toString(), firstName.getText().toString(),
-                        lastName.getText().toString(), "user"
-                );
-
-                dbRef.child(dbRef.push().getKey()).setValue(user);
             }
         });
 
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(!pswChecker(password.getText().toString())){
+                    passwordWarning.setText("");
+                    passwordWarning.setTextColor(getResources().getColor(R.color.red));
+                }else{
+                    passwordWarning.setText("The password looks correct!");
+                    passwordWarning.setTextColor(getResources().getColor(R.color.teal_700));
+                }
+            }
+        });
+
+        // The REGISTER button on click event listener
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
 
     }
+
+    private boolean isLogicalEmail(CharSequence emailToCheck){
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(emailToCheck).matches();
+    }
+
+    public boolean pswChecker(final String psw) {
+
+        Pattern pattern;
+        Matcher matcher;
+
+        /*
+         * ^ means start of string
+         * (?=.*[0-9]) means 1 numeric at least
+         * (?=.*[a-z]) means 1 lowercase at least
+         * (?=.*[A-Z]) means 1 uppercase at least
+         * (?=\S+$) means no spaces/whitespace allowed
+         * .{8,} means 6 characters at least (+start and end of string so + 2 characters)
+         */
+        final String PSW_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{4,}$";
+
+        pattern = Pattern.compile(PSW_PATTERN);
+        matcher = pattern.matcher(psw);
+
+        return matcher.matches();
+    }
+
 }
