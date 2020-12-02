22d35a93fdb2f6fc690cf91ca9c234ebb3fd7611
Vivi <meowsachi@gmail.com>
2020-12-02 19:54:11 +0000

    vivi restaurant list of reviews and card

--- app/src/main/java/com/example/forfoodiesbyfoodies/RestaurantListOfReviews.java
+++ app/src/main/java/com/example/forfoodiesbyfoodies/RestaurantListOfReviews.java
@@ -6,11 +6,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 // This class manages the ability of the app to list the reviews that a restaurant got from Food Critics
-public class RestaurantListOfReviews extends AppCompatActivity {
+public class RestaurantListOfReviews extends AppCompatActivity implements RestaurantListOfReviewsCard.RestaurantReviewHolder.OnCardClickListener{
 
     // Defining the views and other object to be handled later
     Button addReview;
@@ -18,6 +28,10 @@ public class RestaurantListOfReviews extends AppCompatActivity {
     Restaurant restaurant;
     ImageButton delete;
     User user;
+    ArrayList<ReviewTemplate> list = new ArrayList<ReviewTemplate>();
+    DatabaseReference dbref;
+    SfListOfStreetFoodsCard adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +47,10 @@ public class RestaurantListOfReviews extends AppCompatActivity {
         listOfReviews = findViewById(R.id.rv_rest_loreviews_list);
         addReview = findViewById(R.id.btn_rest_loreviews_addnewreview);
         delete = findViewById(R.id.ib_sf_review_card_delete);
+
+        listOfReviews.setLayoutManager(new LinearLayoutManager(RestaurantListOfReviews.this));
+        dbref = FirebaseDatabase.getInstance().getReference("restaurantreviews");
+        dbref.addListenerForSingleValueEvent(listener);
 
         /* If the user that opens this activity is a food critic then show the Write Review button
          * and then setting up a listener for the button to open the Review Writing activity
@@ -52,5 +70,30 @@ public class RestaurantListOfReviews extends AppCompatActivity {
         }
 
 
+    }
+
+    ValueEventListener listener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+            for (DataSnapshot dss : snapshot.getChildren()) {
+                ReviewTemplate rt = dss.getValue(ReviewTemplate.class);
+                list.add(rt);
+            }
+
+            adapter = new RestaurantListOfReviews(list, RestaurantListOfReviews.this);
+            listOfReviews.setAdapter(adapter);
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+    };
+
+    @Override
+    public void onCardClick(int i) {
+        Intent intent = new Intent(RestaurantListOfReviews.this, Profile.class);
+        intent.putExtra("user", user);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/forfoodiesbyfoodies/RestaurantListOfReviewsCard.java
+++ app/src/main/java/com/example/forfoodiesbyfoodies/RestaurantListOfReviewsCard.java
@@ -1,5 +1,71 @@
 package com.example.forfoodiesbyfoodies;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class RestaurantListOfReviewsCard {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.util.ArrayList;
+
+
+public class RestaurantListOfReviewsCard  extends RecyclerView.Adapter<RestaurantListOfReviewsCard.RestaurantReviewHolder> {
+
+    ArrayList<ReviewTemplate> list;
+    RestaurantListOfReviewsCard.RestaurantReviewHolder.OnCardClickListener listener;
+
+    public RestaurantListOfReviewsCard(ArrayList<ReviewTemplate> list, RestaurantListOfReviewsCard.RestaurantReviewHolder.OnCardClickListener listener) {
+        this.list = list;
+        this.listener = listener;
+    }
+
+    public RestaurantListOfReviewsCard() {
+
+    }
+
+    @NonNull
+    @Override
+    public RestaurantListOfReviewsCard.RestaurantReviewHolder onCreateViewHolder(@NonNull ViewGroup view, int viewType) {
+        View content = LayoutInflater.from(view.getContext()).inflate(R.layout.activity_restaurant_list_of_restaurants_card, view, false);
+        RestaurantListOfReviewsCard.RestaurantReviewHolder holder = new RestaurantListOfReviewsCard.RestaurantReviewHolder(content, listener);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RestaurantListOfReviewsCard.RestaurantReviewHolder holder, int position) {
+        holder.writer.setText(list.get(position).getWriter());
+        holder.dateOfVisit.setText(list.get(position).getDateOfVisit());
+        holder.review.setText(list.get(position).getReview());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+    public static class RestaurantReviewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView writer, dateOfVisit, review;
+        RestaurantListOfReviewsCard.RestaurantReviewHolder.OnCardClickListener listener;
+
+        public RestaurantReviewHolder(@NonNull View itemView, RestaurantListOfReviewsCard.RestaurantReviewHolder.OnCardClickListener listener) {
+            super(itemView);
+            writer = itemView.findViewById(R.id.tv_rest_loreview_card_username);
+            review = itemView.findViewById(R.id.tv_rest_loreview_card_experience);
+            dateOfVisit = itemView.findViewById(R.id.tv_rest_loreview_card_date);
+            this.listener = listener;
+            itemView.setOnClickListener(this);
+        }
+
+        public interface OnCardClickListener {
+            public void onCardClick(int i);
+        }
+
+        @Override
+        public void onClick(View v) {
+            this.listener.onCardClick(getAdapterPosition());
+        }
+    }
 
 }
