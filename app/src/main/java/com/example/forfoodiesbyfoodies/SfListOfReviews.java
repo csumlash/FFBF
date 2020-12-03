8cdd3bd86f174c4d6d49f5cb8d3aa44f280fe57f
Vivi <meowsachi@gmail.com>
2020-12-03 00:14:37 +0000

    vivi sf list

--- app/src/main/java/com/example/forfoodiesbyfoodies/SfListOfReviews.java
+++ app/src/main/java/com/example/forfoodiesbyfoodies/SfListOfReviews.java
@@ -5,16 +5,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class SfListOfReviews extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class SfListOfReviews extends AppCompatActivity implements SFListOfReviewsCard.SFReviewHolder.OnCardClickListener {
 
     User user;
     StreetFood streetFood;
 
     Button addReview;
     RecyclerView listOfReviews;
+
+    ArrayList<ReviewTemplate> list = new ArrayList<ReviewTemplate>();
+    DatabaseReference dbref;
+    SFListOfReviewsCard adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +42,10 @@ public class SfListOfReviews extends AppCompatActivity {
         addReview = findViewById(R.id.btn_sf_review_addreview);
         listOfReviews = findViewById(R.id.rv_sf_review_list);
 
+        listOfReviews.setLayoutManager(new LinearLayoutManager(SfListOfReviews.this));
+        dbref = FirebaseDatabase.getInstance().getReference("streetfood");
+        dbref.addListenerForSingleValueEvent(listener);
+
         addReview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,6 +55,31 @@ public class SfListOfReviews extends AppCompatActivity {
                 startActivity(k);
             }
         });
+    }
 
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dss : snapshot.getChildren()) {
+                    ReviewTemplate rt = dss.getValue(ReviewTemplate.class);
+                    list.add(rt);
+                }
+
+                adapter = new SFListOfReviewsCard(list, SfListOfReviews.this);
+                listOfReviews.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+
+
+    @Override
+    public void onCardClick(int i) {
+        Intent intent = new Intent(SfListOfReviews.this, Profile.class);
+        intent.putExtra("user", user);
+        startActivity(intent);
     }
 }
